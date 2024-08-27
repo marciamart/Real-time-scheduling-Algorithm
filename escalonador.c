d93f705ac31c98844f9c1e33a59616b46e78fa5e
marciamart <marciamartinsls05@gmail.com>
2024-08-26 21:34:11 -0300

    Problem

---
+++ escalonador.c
@@ -0,0 +1,12 @@
+//  Crie um programa em C que implemente os algoritmos de escalonamento RM (Rate Monotonic)
+// e EDF (Earliest Deadline First) para sistemas de tempo real. O programa também deve fazer os testes
+// de escalonabilidade e mostrar uma escala de tempo de execução (Semelhantes aos Slides de Aula).
+
+// O programa deve ler um arquivo .txt que possui todas as tarefas de um sistema com os respectivos
+// períodos, tempos de execução no pior caso e deadlines. A seguir temos o exemplo de um arquivo 
+// que mostra um sistema com três tarefas.
+
+// P C D
+// 4 1 4
+// 5 2 5
+// 20 4 20
