994408a2cc5a66b5792588121967654ea9c2d2be
marciamart <marciamartinsls05@gmail.com>
2024-08-27 21:28:43 -0300

    Teste de escalabilidade

--- escalonador.c
+++ escalonador.c
@@ -10,3 +10,27 @@
 // 4 1 4
 // 5 2 5
 // 20 4 20
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Processo{
+    int periodo, capacidade, deadline;
+}Processo;
+
+
+float testeEscalabilidade(Processo processos[], int n){
+    float resultado = 0;
+    for(int i = 0; i < n; i++){
+        resultado += (float)(processos[i].capacidade/processos[i].periodo);
+    }
+    
+    return resultado;
+}
+
+int main (int argc, char *argv[]){
+    FILE *file = fopen(argv[1], "r");
+
+    return 0;
+}
